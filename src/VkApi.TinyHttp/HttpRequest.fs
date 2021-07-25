a4e3981fa16b9bedfba971bc7705d7f453a7524a
aleksandr <a.kolesnikov1989@gmail.com>
2021-07-25 10:10:26 +0300

    Reorganize repo.
    Created VkApi.TinyHttp project.

---
+++ src/VkApi.TinyHttp/HttpRequest.fs
@@ -0,0 +1,43 @@
+﻿namespace VkApi.TinyHttp
+
+open System.IO
+open FSharp.Control.Tasks.V2
+
+
+type Content = {
+    Name: string
+    Content: Stream
+}
+
+type HttpRequest =
+    | Get of string
+    | Post of string * Content
+
+[<RequireQualifiedAccess>]
+module internal Request =
+
+    open System.Net.Http
+
+
+    let private client = new HttpClient ()
+
+    let private GetAsync (url: string) =
+        url
+        |> client.GetStreamAsync
+
+    let private PostAsync (url:string) content =
+        task {
+            let httpContent = new MultipartFormDataContent ()
+            use innerContent = new StreamContent (content.Content)
+            httpContent.Add (innerContent, "file", content.Name)
+            let! response =
+                (url, httpContent)
+                |> client.PostAsync
+
+            return! response.Content.ReadAsStreamAsync ()
+        }
+
+    let SendAsync request =
+        match request with
+        | Get url -> url |> GetAsync
+        | Post (url, content) -> PostAsync url content
---
+++ src/VkApi.TinyHttp/HttpRequestBuilder.fs
@@ -0,0 +1,57 @@
+namespace VkApi.TinyHttp
+
+open System
+
+
+type HttpRequestBuilder () =
+    let MakeGet = Get >> Some
+
+    let MakePost = Post >> Some
+
+    member _.Run value =
+        match value with
+        | None -> raise <| ArgumentException ("Empty Request")
+        | Some request -> Request.SendAsync request
+
+    member _.Yield _ = None
+
+    [<CustomOperation "GET">]
+    member _.Get (value, uri) =
+        match value with
+        | None ->
+            uri |> MakeGet
+        | Some request ->
+            match request with
+            | Get _ ->
+                uri |> MakeGet
+            | Post _ -> raise <| ArgumentException ("Could not change Get Reqeust to Post Reqeust")
+
+    [<CustomOperation "POST">]
+    member _.Post (value, uri) =
+        match value with
+        | None ->
+            (uri, { Name = ""; Content = null })
+            |> MakePost
+        | Some request ->
+            match request with
+            | Get _ -> raise <| ArgumentException ("Could not change Post Reqeust to Get Reqeust")
+            | Post (_, content) ->
+                (uri, content)
+                |> MakePost
+
+    [<CustomOperation "Content">]
+    member _.Content (value, content) =
+        match value with
+        | None ->
+            ("", content)
+            |> MakePost
+        | Some request ->
+            match request with
+            | Get _ -> raise <| ArgumentException ("Could not use Get Request with Content")
+            | Post (uri, _) ->
+                (uri, content)
+                |> MakePost
+
+[<AutoOpen>]
+module HttpRequestBuilderModule =
+    let http = HttpRequestBuilder ()
